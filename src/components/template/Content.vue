9a17e35106c26aa151abde6caaf4b7df4ba07a81
afmdanielwin <danielfarias380@gmail.com>
2020-05-17 16:19:02 -0300

    Content component

---
+++ src/components/template/Content.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="content"></main>
+</template>
+
+<script>
+const Content = {
+    name: 'Content'
+}
+
+export default Content
+</script>
+
+<style>
+    .content {
+        grid-area: content;
+        background-color: #efefef;
+        padding: 20px;
+    }
+</style>
