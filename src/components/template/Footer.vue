78a5f6a390b8d33a58a0a7337f9d1b53c13bf876
afmdanielwin <danielfarias380@gmail.com>
2020-05-17 16:18:45 -0300

    Footer component

---
+++ src/components/template/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="footer">
+      <span>Copyright <strong>Daniel Melo</strong> © 2019-2020</span>
+  </footer>
+</template>
+
+<script>
+const Footer = {
+    name: "Footer"
+}
+
+export default Footer
+</script>
+
+<style>
+    .footer {
+        grid-area: footer;
+        background-color: #DDD;
+        color: #333;
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+</style>
