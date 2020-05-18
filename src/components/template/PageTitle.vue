80a5f1eff197fc81868d4c1ec6f21fbdb3719bfb
afmdanielwin <danielfarias380@gmail.com>
2020-05-17 22:31:56 -0300

    Create PageTitle component

---
+++ src/components/template/PageTitle.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page-title">
+      <h1><i v-if="icon" :class="icon"></i> {{ main }}</h1>
+      <h2>{{ sub }}</h2>
+      <hr>
+  </div>
+</template>
+
+<script>
+const PageTitle = {
+    name: 'PageTitle',
+    props: ['icon', 'main', 'sub']
+}
+
+export default PageTitle
+</script>
+
+<style>
+    .page-title h1 {
+        margin: 0px;
+    }
+    
+    .page-title h1 {
+        color: #777;
+        margin-top: 5px;
+        font: 1.3rem
+    }
+</style>
