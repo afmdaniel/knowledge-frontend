5502189780d3fc43d8f4ccd244a10f7f985b3828
afmdanielwin <danielfarias380@gmail.com>
2020-05-17 22:40:05 -0300

    Create Home component

---
+++ src/components/home/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+        <PageTitle icon="fa fa-home" main="Dashboard" sub="Base de Conhecimento" />
+    </div>
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle'
+
+const Home = {
+    name: 'Home',
+    components: {
+        PageTitle
+    }
+}
+
+
+export default Home
+</script>
+
+<style>
+
+</style>
