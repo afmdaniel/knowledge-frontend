0f26cd2e3a98b414177a36b24bcec7ada083b485
afmdanielwin <danielfarias380@gmail.com>
2020-05-22 23:31:09 -0300

    Handle token expiration

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,18 @@
 import axios from 'axios'
 
+const success = res => res
+const error = err => {
+    if (401 === err.response.status) {
+        window.location = '/'
+    } else {
+        return Promise.reject(err)
+    }
+}
+
 const api = axios.create({ 
     baseURL: 'http://localhost:3000'
- })
+})
+
+api.interceptors.response.use(success, error)
 
  export default api
